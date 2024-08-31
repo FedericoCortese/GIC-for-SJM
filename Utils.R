52ae0468cf47ed06fe769e3ed933945d1987d19c
Federico P. Cortese <67327161+FedericoCortese@users.noreply.github.com>
2024-08-31 11:21:46 +0200

    mods

--- Utils.R
+++ Utils.R
@@ -1,16 +1,26 @@
-library(RcppHMM)
-library(reticulate)
-library(pdfCluster)
-library(boot)
-library(xtable)
-#py_install("scipy")
+# Function to check if a package is installed and install it if necessary
+check_and_install <- function(pkg) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg)
+  }
+  library(pkg, character.only = TRUE)
+}
+
+# Check and install R packages
+check_and_install("RcppHMM")
+check_and_install("reticulate")
+check_and_install("pdfCluster")
+check_and_install("boot")
+check_and_install("xtable")
 
 library(RcppHMM)
 library(reticulate)
 library(pdfCluster)
 library(boot)
 library(xtable)
-#py_install("scipy")
+
+# Run the following only one time to install the python package
+# py_install("scipy")
 
 # Import the python module
 import("scipy")
@@ -40,7 +50,7 @@ order_states=function(states){
   return(states_temp)
 }
 
-SJM_est=function(Y,K,kappa,lambda,Ksat=6,alpha0,K0,pers0){
+SJM_est=function(Y,K,kappa,lambda,Ksat=6,alpha0=NULL,K0=NULL,pers0=NULL){
   
   # This function estimates the parameters of the SJM model
   
@@ -66,6 +76,18 @@ SJM_est=function(Y,K,kappa,lambda,Ksat=6,alpha0,K0,pers0){
   library(dplyr)
   library(tidyverse)
   library(tidyr)
+  
+  if(is.null(alpha0)){
+    alpha0=dim(Y)[2]
+  }
+  
+  if(is.null(K0)){
+    K0=K
+  }
+  
+  if(is.null(pers0)){
+    pers0=.95
+  }
   
   YY=apply(Y,2,scale)
   PP=dim(Y)[2]
@@ -222,276 +244,3 @@ sim_data=function(seed,Ktrue,N,P,cors,pers,m){
               YY=YY))
   
 }
-
-sim_est_SJM=function(seed,K,lambda,kappa,true_data,Ktrue,
-                     N=1000,P=100,m=2,cors,pers,K0,alpha0){
-  
-  # Function to estimate the parameters of a sparse jump model
-  # seed is the seed for the random number generator
-  # K is the number of states
-  # lambda is the penalty for the jump
-  # kappa is the penalty for sparsity
-  # true_data is obtained using the function "sim_data"
-  # Ktrue is the true number of states
-  # N is the number of observations
-  # P is the number of features
-  # m is an integer number such that m*P is the number of false features
-  # cors is a vector with correlations, one for each state
-  # pers is the self-transition probability
-  # K0 is the prior number of states
-  
-  
-  # Output: 
-  # A list with the estimated weights,
-  # the normalized estimated weights,
-  # the estimated states,
-  # the seed,
-  # the number of observations,
-  # the number of features,
-  # the number of false features,
-  # the correlations,
-  # the self-transition probability,
-  # the true number of states,
-  # the penalty for the jump,
-  # the penalty for sparsity,
-  # the L1 norm of the BCSS,
-  # the L2 norm of the BCSS,
-  # the penalty for the number of jumps,
-  # the number of selected features,
-  # the adjusted Rand index for the states,
-  # the adjusted Rand index for the weights,
-  # the number of correctly selected features,
-  # the number of incorrectly selected features
-  
-  
-  YY=true_data[[seed]]$YY
-  true_states=true_data[[seed]]$true_states
-  
-  res=sparse_jump(Y=as.matrix(YY), n_states=as.integer(K), 
-                  max_features=kappa, 
-                  jump_penalty=lambda,
-                  max_iter=as.integer(10), 
-                  tol=1e-4, 
-                  n_init=as.integer(10), 
-                  verbose=F)
-  
-  est_weights=res[[2]]
-  norm_est_weights=est_weights/sum(est_weights)
-  
-  est_states=order_states(res[[1]])
-  true_states=order_states(true_states)
-  
-  Pfalse=dim(YY)[2]-P
-  true_weights_seq=c(rep(1,P),rep(0,Pfalse))
-  est_weights_seq=as.numeric(est_weights!=0)
-  
-  ARI_states=adj.rand.index(true_states,est_states)
-  ARI_weights=adj.rand.index(true_weights_seq,est_weights_seq)
-  
-  corr_sel_feat=sum(est_weights[1:P]!=0)
-  wrong_sel_feat=sum(est_weights[-(1:P)]!=0)
-  
-  indx=which( est_weights!=0)
-  XX=YY[,indx]
-  ### First version: compute BCSS as L1 norm
-  Ln1=sum(get_BCSS(as.matrix(XX),est_states))
-  ### Second version: compute BCSS as L2 norm
-  Ln2=sqrt(sum(get_BCSS(as.matrix(XX),est_states)^2))
-  
-  CKp=-log(K)-log(2*pi)*kappa/2
-
-  loglik=N*CKp-Ln1/2
-  
-  pen=sum(est_states[1:(N-1)]!=est_states[2:N])
-  alphak=length(which(est_weights!=0))
-  
-  # TotalPenalty=K0*alpha0+alpha0*(K-K0)+K0*(alphak-alpha0)+pen
-  
-  return(list(
-    #YY=YY,
-    indx=indx,
-    est_weights=est_weights,
-    norm_est_weights=norm_est_weights,
-    est_weights_seq=est_weights_seq,
-    est_states=est_states,
-    true_states=true_states,
-    seed=seed,
-    N=N,
-    P=P,
-    m=m,
-    cors=cors,
-    pers=pers,
-    K=K,
-    lambda=lambda,
-    kappa=kappa,
-    Ln1=Ln1,
-    Ln2=Ln2,
-    loglik=loglik,
-    pen=pen,
-    alphak=alphak,
-    ARI_states=ARI_states,
-    ARI_weights=ARI_weights,
-    corr_sel_feat=corr_sel_feat,
-    wrong_sel_feat=wrong_sel_feat))
-  
-}
-
-
-satmod_est=function(seed,true_data,Ksat=6){
-  
-  # Function to estimate the parameters of a saturated model
-  # seed is the seed for the random number generator
-  # true_data is obtained using the function "sim_data"
-  # Ksat is the number of states for the saturated model
-  
-  # Output:
-  # A list with the L1 and L2 norms of the BCSS, 
-  # the estimated weights, 
-  # the normalized estimated weights,
-  # and the estimated states
-  
-  YY=true_data[[seed]]$YY
-  true_states=true_data[[seed]]$true_states
-  
-  res_sat=sparse_jump(Y=as.matrix(YY), n_states=as.integer(Ksat), 
-                      max_features=sqrt(dim(YY)[2]), 
-                      jump_penalty=0,
-                      max_iter=as.integer(10), 
-                      tol=1e-4, 
-                      n_init=as.integer(10), 
-                      verbose=F)
-  
-  est_weights_sat=res_sat[[2]]
-  norm_est_weights_sat=est_weights_sat/sum(est_weights_sat)
-  
-  est_states_sat=order_states(res_sat[[1]])
-  
-  ### First version: compute BCSS as L1 norm
-  Lnsat1=sum(get_BCSS(as.matrix(YY),est_states_sat))
-  ### Second version: compute weighted BCSS (some features will disappear)
-  Lnsat2=sqrt(sum(get_BCSS(as.matrix(YY),est_states_sat)^2))
-  
-  return(list(Lnsat1=Lnsat1,
-              Lnsat2=Lnsat2,
-              est_weights_sat=est_weights_sat,
-              norm_est_weights_sat=norm_est_weights_sat,
-              est_states_sat=est_states_sat))
-  
-}
-
-GIC=function(simest,satmod,Ksat=6,alpha0,K0){
-  
-  # Function to compute the GICs for the sparse jump model
-  # simest is a list of the results of the sparse jump model estimated for varying parameters (lambda, kappa, K)
-  # satmod is a list of the results of the saturated model estimated for varying parameters (lambda, kappa, K)
-  # Ksat is the number of states for the saturated model
-  # alpha0 is prior number of features  
-  # K0 is prior number of states
-  
-  # Output:
-  # A data frame with the average and standard deviation of the GICs for the sparse jump model
-  
-  library(dplyr)
-  library(tidyverse)
-  library(tidyr)
-  
-  K=unlist(lapply(simest,function(x)x$K))
-  kappa=unlist(lapply(simest,function(x)x$kappa))
-  lambda=unlist(lapply(simest,function(x)x$lambda))
-  #YY=unlist(lapply(simest,function(x)x$K))
-  PP=unlist(lapply(simest,function(x)x$P))
-  PP=PP*(1+unlist(lapply(simest,function(x)x$m)))
-  N=unlist(lapply(simest,function(x)x$N))
-  seed=unlist(lapply(simest,function(x)x$seed))
-  ARI.weights=unlist(lapply(simest,function(x)x$ARI_weights))
-  ARI.states=unlist(lapply(simest,function(x)x$ARI_states))
-  pen=unlist(lapply(simest,function(x)x$pen))
-  
-  # if(drop_pterm){
-  #   CKp=-log(K)-log(2*pi)*1/2
-  #   CKp_sat=-log(Ksat)-log(2*pi)*1/2
-  # }
-  # else{
-  CKp=-log(K)-log(2*pi)*kappa/2
-  CKp_sat=-log(Ksat)-log(2*pi)*sqrt(PP)/2
-  #}
-  
-  anFTIC=log(log(N))*log(PP)
-  anAIC=rep(2,length(N))
-  anBIC=log(N)
-  
-  alphak=unlist(lapply(simest,function(x)x$alphak))
-  pers=unlist(lapply(simest,function(x)x$pers))
-  pen0=(1-pers)*N*(K0-1)
-  
-  TotalPenalty=(alpha0+pen0)*K+K0*(alphak-alpha0+pen-pen0) 
-  
-  Klk=length(unique(K))*length(unique(kappa))*length(unique(lambda))
-  setup=rep(1:Klk,
-            each=length(unique(seed)))
-  
-  Ln=unlist(lapply(simest,function(x)x$Ln1))
-  Lnsat=unlist(lapply(satmod,function(x)x$Lnsat1))
-  Lnsat=(rep(Lnsat,Klk))
-  Ln_diff=Lnsat-Ln
-  CKp_diff=CKp_sat-CKp
-  
-  FTIC=2*CKp_diff+(Ln_diff+anFTIC*TotalPenalty)/N
-  BIC=2*CKp_diff+(Ln_diff+anBIC*TotalPenalty)/N
-  AIC=2*CKp_diff+(Ln_diff+anAIC*TotalPenalty)/N
-  
-  corr_sel_feat=unlist(lapply(simest,function(x)x$corr_sel_feat))
-  wrong_sel_feat=unlist(lapply(simest,function(x)x$wrong_sel_feat))
-  est_weights=data.frame(matrix(unlist(lapply(simest,function(x)x$norm_est_weights)),
-                                ncol = PP[1],byrow=T))
-  
-  loglik=unlist(lapply(simest,function(x)x$loglik))
-  
-  res=data.frame(seed,
-                 FTIC,
-                 BIC,
-                 AIC,
-                 Ln_diff,
-                 CKp_diff,
-                 pen,
-                 lambda,
-                 kappa,
-                 K,
-                 ARI.states,
-                 ARI.weights,
-                 setup,
-                 corr_sel_feat,
-                 wrong_sel_feat,
-                 loglik)
-  
-  res_est_weights=data.frame(setup,
-                             est_weights)
-  
-  res=res%>%group_by(setup)%>%
-    summarise(avFTIC=mean(FTIC),
-              avAIC=mean(AIC),
-              avBIC=mean(BIC),
-              sdFTIC=sd(FTIC),
-              sdAIC=sd(AIC),
-              sdBIC=sd(BIC),
-              avARI_states=mean(ARI.states),
-              avARI_weights=mean(ARI.weights),
-              avLn_diff=mean(Ln_diff),
-              avCKp_diff=mean(CKp_diff),
-              avPen=mean(pen),
-              lambda=mean(lambda),
-              kappa=mean(kappa),
-              K=mean(K),
-              corr_sel_feat=mean(corr_sel_feat),
-              wrong_sel_feat=mean(wrong_sel_feat),
-              avloglik=mean(loglik))
-  
-  res_est_weights=res_est_weights%>%group_by(setup)%>%
-    summarise_all(mean)
-  
-  res_av=data.frame(res,res_est_weights[,-1])
-  
-  return(res_av)
-}
-
-
--- est_example.R
+++ est_example.R
@@ -1,36 +1,39 @@
 source("Utils.R")
 
 #Simulate data
-pers1=.99
+pers=.99
 Ns=600
 seed=123
 corsK2=c(.8,.4)
 P=100
 
-Y=sim_data(seed,Ktrue=2,Ns,P,corsK2,pers1,m=1)
+Y=sim_data(seed,Ktrue=2,Ns,P,corsK2,pers,m=1)
 
 true_stats=Y[[1]]
 Y=Y[[2]]
 
 # Estimate SJM with varying lambda and kappa
-Yest_0=SJM_est(Y,K=3,kappa=2,lambda=0,
-             Ksat=6,alpha0=100,K0=2,pers0=.95)
-Yest_1=SJM_est(Y,K=2,kappa=7,lambda=10,
-               Ksat=6,alpha0=100,K0=2,pers0=.95)
-Yest_2=SJM_est(Y,K=2,kappa=16,lambda=50,
-               Ksat=6,alpha0=100,K0=2,pers0=.95)
+Yest_0=SJM_est(Y,K=3,kappa=2,lambda=0)
+Yest_1=SJM_est(Y,K=2,kappa=7,lambda=10)
+Yest_2=SJM_est(Y,K=2,kappa=16,lambda=50)
 
 # Compare FTIC
 Yest_0$FTIC
 Yest_1$FTIC
 Yest_2$FTIC
 
+# Best model according to FTIC: Yest_1
+
 # Compare in terms of ARI computed between true and estimated states
 pdfCluster::adj.rand.index(Yest_0$est_states,true_stats)
 pdfCluster::adj.rand.index(Yest_1$est_states,true_stats)
 pdfCluster::adj.rand.index(Yest_2$est_states,true_stats)
 
+# Best model according to ARI: Yest_1 and Yest_2
+
 # Selected features (the first P are true features)
 which(Yest_0$est_weights!=0)
 which(Yest_1$est_weights!=0)
 which(Yest_2$est_weights!=0)
+
+# Best model according to correctly selected features: Yest_1
